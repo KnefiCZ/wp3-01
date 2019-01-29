029f13a2ce97c59fd3a48dca46cb9c85c9bae140
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-01-29 15:19:17 +0100

    Update index.php
    HOTOVO:()))))))))))))))))))))))))))))))))

--- index.php
+++ index.php
@@ -5,14 +5,30 @@
     <title>ay:-)</title>
   </head>
   <body>
+
     <?php
-    $V = 0;
-    do {
+    $objem = 0;
+    echo "Prázdný objem:";
+    while ($objem < 60){
       $naklad = rand(1,5);
-      $V += $naklad;
-      echo "Přidáno: " . $V . " tun nákladu. <br>";
-    } while ($V <= 60);
-      echo "Jsem naložený, odjíždím.";
+      ?>  <div style="display: block; border: 1px solid #000; width: 60px; height: 10px;">
+          <div style="display: block; width:<?php echo $objem; ?>px; height: 10px; background-color: green;">
+            </div>
+            </div>
+    <?php  if  (($objem + $naklad) >= 60) {
+        $naklad= 60 - $objem;
+      }
+      echo "Přidáno: " . $naklad . " tun nákladu. <br>";
+      ?>  <?php
+      $objem += $naklad;
+    }
+    ?>
+    <div style="display: block; border: 1px solid #000; width: 60px; height: 10px;">
+        <div style="display: block; width:60px; height: 10px; background-color: green;">
+        </div>
+    </div>
+    <?php
+    echo "Jsem naložený: " . $objem . " a odjíždím.";
      ?>
   </body>
   </html>
